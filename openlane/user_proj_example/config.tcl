216377072b2d777e88852959b38d96483a4766e1
BYCakar <bycakar98@gmail.com>
2021-12-31 17:31:13 +0300

    Add GDS and LEF

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -42,11 +42,13 @@ set ::env(CLOCK_PERIOD) "20"
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) "0.3"
-set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) "0.3"
+#set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) "0.3"
+#set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) "0.3"
+
+set ::env(ROUTING_CORES) "8"
 
 #set ::env(PL_BASIC_PLACEMENT) 1
-#set ::env(PL_TARGET_DENSITY) 0.05
+set ::env(PL_TARGET_DENSITY) 0.60
 
 # Maximum layer used for routing is metal 4.
 # This is because this macro will be inserted in a top level (user_project_wrapper) 
@@ -58,6 +60,8 @@ set ::env(GLB_RT_MAXLAYER) 5
 set ::env(VDD_NETS) [list {vccd1}]
 set ::env(GND_NETS) [list {vssd1}]
 
-set ::env(DIODE_INSERTION_STRATEGY) 3 
+set ::env(DIODE_INSERTION_STRATEGY) 3
+set ::env(GLB_RT_ANT_ITERS) 8 
+set ::env(GLB_RT_MAX_DIODE_INS_ITERS) 10
 # If you're going to use multiple power domains, then disable cvc run.
 set ::env(RUN_CVC) 1
